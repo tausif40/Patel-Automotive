5c966dfd2b0cc31c208701cb19331aaaa0d5eb73
Mohd Tausif <tausif6040@gmail.com>
2024-05-15 21:31:48 +0530

    Update app.url.js

--- React-UI/src/app.url.js
+++ React-UI/src/app.url.js
@@ -1,13 +1,5 @@
-export const userUrl = "https://api.patelautomotive.in/user/";
-export const categoryUrl = "https://api.patelautomotive.in/category/";
-export const subCategoryUrl = "https://api.patelautomotive.in/subcategory/";
-export const productUrl = "https://api.patelautomotive.in/product/";
-export const wishlistUrl = "https://api.patelautomotive.in/wishlist/";
-
-
-
-// export const userUrl = "http://localhost:8000/user/";
-// export const categoryUrl = "http://localhost:8000/category/";
-// export const subCategoryUrl = "http://localhost:8000/subcategory/";
-// export const productUrl = "http://localhost:8000/product/";
-// export const wishlistUrl = "http://localhost:8000/wishlist/";
+export const userUrl = "http://localhost:8000/user/";
+export const categoryUrl = "http://localhost:8000/category/";
+export const subCategoryUrl = "http://localhost:8000/subcategory/";
+export const productUrl = "http://localhost:8000/product/";
+export const wishlistUrl = "http://localhost:8000/wishlist/";
